54b763b32e67f8c2d02d6277072a9043fc083e63
autoaidc6 <autoaidc6@gmail.com>
2025-10-24 21:48:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-        allowedHosts: 'biolingo.onrender.com',
+        allowedHosts: ['biolingo.onrender.com'],
       },
       plugins: [react()],
       define: {
